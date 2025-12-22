b053e562edfc94cd4ee8927788cc84f5b534d140
Tiru vardhan <150202554+tiru02@users.noreply.github.com>
2025-12-22 11:31:03 +0530

    fixing issues related to sockets and admin access

--- propForm/src/app.js
+++ propForm/src/app.js
@@ -5,10 +5,9 @@ import bodyParser from 'body-parser'
 import fs, { readFileSync } from 'fs'
 import session from 'express-session'
 import cookieParser from 'cookie-parser'
-import { WebSocketServer } from 'ws'
 const dirName = path.resolve(), app = express(), viewsPath = path.join(dirName, 'assets/views')
-var arr = JSON.parse(fs.readFileSync(path.join(dirName, '/data/students.json'), 'utf-8')),
-    arr1 = JSON.parse(fs.readFileSync(path.join(dirName, '/data/faculty.json'), 'utf-8')),
+var arr = JSON.parse(fs.readFileSync(path.join(dirName, '/tmp/students.json'), 'utf-8')),
+    arr1 = JSON.parse(fs.readFileSync(path.join(dirName, '/tmp/faculty.json'), 'utf-8')),
     dataX = {
         students: '',
         faculty: '',
@@ -16,11 +15,10 @@ var arr = JSON.parse(fs.readFileSync(path.join(dirName, '/data/students.json'), 
     },
     PORT = process.env.PORT || 3000,
     partialPath = path.join(dirName, 'assets/partials'),
-    ipAddr = JSON.parse(readFileSync(path.join(dirName, '/data/trusted.json'), 'utf-8')),
+    ipAddr = JSON.parse(readFileSync(path.join(dirName, '/tmp/trusted.json'), 'utf-8')),
     appServer = app.listen(PORT, () => {
         console.log(`running at http://localhost:${PORT}`)
-    }),
-    socketServer = new WebSocketServer({ port: 443 })
+    })
 hbs.registerPartials(partialPath, err => console.log((err) ? err : ''))
 app.use(express.static(path.join(dirName, 'public')))
 app.use(express.json())
@@ -56,7 +54,7 @@ app.get('/login', (req, res) => {
     res.render('login')
 })
 app.post('/login', (req, res) => {
-    var passes = JSON.parse(fs.readFileSync(path.join(dirName, '/data/secured.json'), 'utf-8'))
+    var passes = JSON.parse(fs.readFileSync(path.join(dirName, '/tmp/secured.json'), 'utf-8'))
     req.session.userId = req.body.username
     passes.forEach(elem => {
         if (elem.username == req.body.username) {
@@ -86,7 +84,7 @@ app.post('/login', (req, res) => {
                 dataX.username = `${req.body.username}`
                 res.render('admin', dataX)
             } else {
-                res.redirect('/login')
+                res.redirect('/')
                 dataX.allow = false
             }
         }
@@ -97,7 +95,7 @@ app.get('/logout', (req, res) => {
         if (err)
             res.send('failed to logout')
         else
-            res.redirect('/login')
+            res.redirect('/')
     })
 })
 app.post('/submit-student-complaint', (req, res) => {
@@ -137,14 +135,4 @@ app.get('*', (req, res) => {
     res.render('error', {
         message: 'Oops! The page you are looking for does not exist.'
     })
-})
-socketServer.on('connection', ws => {
-    ws.on('message', data => {
-        socketServer.clients.forEach(client => {
-            client.send(`${data}`)
-        })
-    })
-    ws.onerror = () => {
-        console.log('websocket error')
-    }
 })
